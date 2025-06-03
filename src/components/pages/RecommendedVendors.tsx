d82fe7f81dbb4acd939d2693a8a4c0049bd6883d
fayad  Mhamid <fayad6@walla.co.il>
2025-06-03 19:57:39 +0300

    updates

--- src/components/pages/RecommendedVendors.tsx
+++ src/components/pages/RecommendedVendors.tsx
@@ -1,4 +1,5 @@
-import React, {FunctionComponent, useEffect, useState} from "react";
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import  {FunctionComponent, useEffect, useState} from "react";
 import {
 	Grid,
 	Typography,
